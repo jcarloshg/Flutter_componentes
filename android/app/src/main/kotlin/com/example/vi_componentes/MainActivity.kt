0c2d66ba1769e7d7f318458303a6948c1fa73331
jcarloshg <carlosj12336@gmail.com>
2021-12-13 18:52:40 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/vi_componentes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vi_componentes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_temp.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  HomePageTemp({Key? key}) : super(key: key);
+
+  final opciones = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Componentes temp"),
+      ),
+      body: ListView(
+        children: _crearItemsCorta(),
+      ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = [];
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(title: Text(opt));
+
+      lista
+        ..add(tempWidget)
+        ..add(const Divider());
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones
+        .map((opcion) => Column(
+              children: [
+                ListTile(
+                  title: Text(opcion),
+                  subtitle: const Text("hola mi pana"),
+                  leading: const Icon(Icons.place_sharp),
+                  trailing: const Icon(Icons.downhill_skiing),
+                  onTap: () => print("Hola mi pana"),
+                ),
+                const Divider(),
+              ],
+            ))
+        .toList();
+  }
+}
