406b9447d0ec333977f6b5a10140c5fa991d4264
jcarloshg <carlosj12336@gmail.com>
2021-12-15 18:28:03 -0600

    add animated contaner

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:vi_componentes/pages/alert_page.dart';
+import 'package:vi_componentes/pages/animated_container.dart';
 import 'package:vi_componentes/pages/avatar_page.dart';
 import 'package:vi_componentes/pages/card_page.dart';
 import 'package:vi_componentes/pages/home_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
   return <String, WidgetBuilder>{
-    '/'     : (BuildContext context) => const HomePage(),
-    'alert' : (BuildContext context) => const AlertPage(),
+    '/': (BuildContext context) => const HomePage(),
+    'alert': (BuildContext context) => const AlertPage(),
     'avatar': (BuildContext context) => const AvatarPage(),
-    'card'  : (BuildContext context) => const CardPage(),
+    'card': (BuildContext context) => const CardPage(),
+    'animatedContainer': (BuildContext context) =>
+        const AnimatedContainerPage(),
   };
 }
